70744488e2b1ab2de26e3bfb2fd7b8263817507c
Abhijit Guha Roy <abhi4ssj@gmail.com>
2018-01-17 16:06:04 +0100

    Update RunFile.m

--- RunFile/RunFile.m
+++ RunFile/RunFile.m
@@ -7,7 +7,7 @@ close all
 % Enter the path to your data
 DataPath = '../SampleData/';
 % FileName
-FileName = 'SampleData.mgz';
+FileName = 'SampleData.mgz'; % This is a sample Data from OASIS Dataset not used in training the model
 % Use your Loader to load MRI of isotropic resolution (I use MRIread from FreeSurfer)
 % The volume has to be resampled to isotropic resolution (256*256*256) with Coronal axis as dimension one i.e (dimension 1 and 2 provides coronal slices)
 % I recommend 'mri-convert --conform' routine in FreeSurfer to pre-process which takes less than a second.
